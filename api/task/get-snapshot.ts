8cf6453a07296a9ca4d9f677b4387fc53b00ab38
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-02-12 01:40:41 +0200

    + adjust to latest structure

--- api/task/get-snapshot.ts
+++ api/task/get-snapshot.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { StorageSingleton } from "../../storage/storage-singleton";
+import { JwtPayload } from "jsonwebtoken";
 
 export type GetSnapshotPayload = {
     userId: string
@@ -19,23 +20,32 @@ function isGetSnapshotPayload(data: any): data is GetSnapshotPayload {
     return true;
 }
 
-export async function handleGetSnapshotRequest(request: Request, response: Response) {
+export async function handleGetSnapshotRequest(token: JwtPayload, request: Request, response: Response) {
     let data = request.body;
 
+    if (!token.sub) {
+        console.warn("missing 'sub' from token");
+        response.status(500).send("error: missing 'sub' from token");
+        return;
+    }
+
     if (!isGetSnapshotPayload(data)) {
+        console.log("incorrect request");
         response.status(400).send("error: incorrect request");
         return;
     }
 
     let storage = await StorageSingleton.getInstance();
 
-    let result = await storage.TaskRepository?.getSnapshot(data.userId);
+    let result = await storage.TaskRepository?.getSnapshot(token.sub);
 
-    if (result) {
-        response.status(200).send(JSON.stringify(result));
-    } else {
+    if (!result) {
         response.status(500).send();
         console.warn(`[RegisterAction] Failed to register action.
          { userId: '${data.userId}'`);
+
+        return;
     }
+
+    response.status(200).send(JSON.stringify(result));
 }
