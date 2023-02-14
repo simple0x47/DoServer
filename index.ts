8afc3216b84eee1c73804ee53bf9a937d85d84d8
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-02-14 20:10:14 +0200

    + https certificate & private key

--- index.ts
+++ index.ts
@@ -3,7 +3,13 @@ import bodyParser from 'body-parser';
 import { initializeTaskApi } from './api/task/init';
 import cors from 'cors';
 import https from 'https';
+import fs from 'fs';
 import { initializeAuthApi } from './api/user/init';
+
+const privateKey = fs.readFileSync(process.env["HTTPS_PRIVATE_KEY_FILE_PATH"]!, 'utf8');
+const certificate = fs.readFileSync(process.env["HTTPS_CERTIFICATE_FILE_PATH"]!, 'utf8');
+
+const httpsCredentials = { key: privateKey, cert: certificate };
 
 const app: Express = express();
 const port = 8020;
@@ -17,7 +23,7 @@ app.use(cors({
 initializeAuthApi(app);
 initializeTaskApi(app);
 
-https.createServer(app)
+https.createServer(httpsCredentials, app)
     .listen(port, () => {
         console.log(`[server]: server running at https://localhost:${port}`);
     });
